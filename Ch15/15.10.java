b5e0572c108bc444d28f6e0de798e1b19a97b68d
dales <dalesimmons0210@gmail.com>
2021-07-05 22:09:41 -0700

    ch 15

---
+++ Ch15/15.10.java
@@ -0,0 +1,33 @@
+/* Write a method longestSortedSequence that returns the length of the longest sorted sequence within a list of integers. For example, if a variable called list stores the following sequence of values:
+
+       
+[1, 3, 5, 2, 9, 7, -3, 0, 42, 308, 17]
+then the call: list.longestSortedSequence() would return the value 4 because it is the length of the longest sorted sequence within this list (the sequence -3, 0, 42, 308). If the list is empty, your method should return 0. Notice that for a non-empty list the method will always return a value of at least 1 because any individual element constitutes a sorted sequence.
+
+Assume you are adding to the ArrayIntList class with following fields:
+
+public class ArrayIntList {
+    private int[] elementData;
+    private int size;
+
+    // your code goes here
+} */
+public int longestSortedSequence() {
+   if (size == 0) {
+       return 0;
+   }  
+   int max = 1;
+   int length = 1;
+    
+   for (int i = 0; i < size; i++) {
+      if (elementData[i] <= elementData[i + 1]) {
+         length++; 
+      } else {
+         if (length > max) {
+            max = length;
+         } 
+         length = 1; 
+      } 
+   }  
+   return Math.max(length, max);
+}
